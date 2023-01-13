ee695a5bdf6fe738e4dfe9e800503db045b04108
zabierowski <zabierowski@nomachine.com>
2023-01-13 13:53:54 +0100

    added working front

--- src/quickQuiz.js
+++ src/quickQuiz.js
@@ -1,141 +1,131 @@
-import './quickQuiz.scss';
+import "./quickQuiz.scss";
 import {
-	TextControl,
-	Flex,
-	FlexBlock,
-	FlexItem,
-	Button,
-	Icon,
-} from '@wordpress/components';
+  TextControl,
+  Flex,
+  FlexBlock,
+  FlexItem,
+  Button,
+  Icon,
+} from "@wordpress/components";
 
-wp.blocks.registerBlockType('quizblock/mainquiz', {
-	title: 'Quiz Block',
-	description: 'My Quiz Block',
-	edit: EditComponent,
-	save: SaveComponent,
-	attributes: {
-		Question: { type: 'string' },
-		AnswersArray: { type: 'array', default: [] },
-		CorrectAnswer: { type: 'number', default: -1 },
-	},
+wp.blocks.registerBlockType("quizblock/mainquiz", {
+  title: "Quiz Block",
+  description: "My Quiz Block",
+  edit: EditComponent,
+  save: SaveComponent,
+  attributes: {
+    Question: { type: "string" },
+    AnswersArray: { type: "array", default: [] },
+    CorrectAnswer: { type: "number", default: -1 },
+  },
 });
 
 function EditComponent(props) {
-	const onAddAnswerClick = () => {
-		props.setAttributes({
-			AnswersArray: [...props.attributes.AnswersArray, ''],
-		});
-	};
-	const onQuestionChange = (value) => {
-		props.setAttributes({ Question: value });
-	};
+  const onAddAnswerClick = () => {
+    props.setAttributes({
+      AnswersArray: [...props.attributes.AnswersArray, ""],
+    });
+  };
+  const onQuestionChange = (value) => {
+    props.setAttributes({ Question: value });
+  };
 
-	const onDeleteClick = (answerIndex) => {
-		let newArray = props.attributes.AnswersArray;
-		let newAnswer;
+  const onDeleteClick = (answerIndex) => {
+    let newArray = props.attributes.AnswersArray;
+    let newAnswer;
 
-		if (props.attributes.CorrectAnswer === answerIndex) {
-			newAnswer = -1;
-		} else if (
-			props.attributes.CorrectAnswer > answerIndex &&
-			props.attributes.CorrectAnswer !== -1
-		) {
-			newAnswer = props.attributes.CorrectAnswer - 1;
-		} else {
-			newAnswer = props.attributes.CorrectAnswer;
-		}
+    if (props.attributes.CorrectAnswer === answerIndex) {
+      newAnswer = -1;
+    } else if (
+      props.attributes.CorrectAnswer > answerIndex &&
+      props.attributes.CorrectAnswer !== -1
+    ) {
+      newAnswer = props.attributes.CorrectAnswer - 1;
+    } else {
+      newAnswer = props.attributes.CorrectAnswer;
+    }
 
-		newArray = newArray
-			.slice(0, answerIndex)
-			.concat(newArray.slice(answerIndex + 1, newArray.length));
-		props.setAttributes({
-			AnswersArray: newArray,
-			CorrectAnswer: newAnswer,
-		});
-	};
+    newArray = newArray
+      .slice(0, answerIndex)
+      .concat(newArray.slice(answerIndex + 1, newArray.length));
+    props.setAttributes({
+      AnswersArray: newArray,
+      CorrectAnswer: newAnswer,
+    });
+  };
 
-	return (
-		<div className="quizWrapper">
-			<TextControl
-				label="Quiz Question:"
-				className="questionText"
-				value={props.attributes.Question}
-				onChange={onQuestionChange}
-			/>
-			<p className="answersHeader">Answers:</p>
-			{[...Array(props.attributes.AnswersArray.length)].map(
-				(answer, answerIndex) => {
-					return (
-						<Flex key={answerIndex}>
-							<FlexBlock>
-								<TextControl
-									className="answerText"
-									value={
-										props.attributes.AnswersArray[
-											answerIndex
-										]
-									}
-									onChange={(newValue) => {
-										const newArray =
-											props.attributes.AnswersArray.concat(
-												[]
-											);
-										newArray[answerIndex] = newValue;
-										props.setAttributes({
-											AnswersArray: newArray,
-										});
-									}}
-								/>
-							</FlexBlock>
-							<FlexItem>
-								<Button
-									className={
-										'iconButton' +
-										(props.attributes.CorrectAnswer ===
-										answerIndex
-											? ' correctAnswer '
-											: '')
-									}
-									onClick={() => {
-										props.setAttributes({
-											CorrectAnswer: answerIndex,
-										});
-									}}
-								>
-									<Icon
-										className="selectAnswerIcon"
-										icon="marker"
-									/>
-								</Button>
-							</FlexItem>
-							<FlexItem>
-								<Button
-									className="iconButton"
-									onClick={() => {
-										onDeleteClick(answerIndex);
-									}}
-								>
-									<Icon
-										className="deleteAnswerIcon"
-										icon="trash"
-									/>
-								</Button>
-							</FlexItem>
-						</Flex>
-					);
-				}
-			)}
-			<Button isPrimary onClick={onAddAnswerClick}>
-				Add another answer
-			</Button>
-		</div>
-	);
+  return (
+    <div className="quickQuiz_quizEditWrapper">
+      <TextControl
+        label="Quiz Question:"
+        className="questionText"
+        value={props.attributes.Question}
+        onChange={onQuestionChange}
+      />
+      <p className="answersHeader">Answers:</p>
+      {[...Array(props.attributes.AnswersArray.length)].map(
+        (answer, answerIndex) => {
+          return (
+            <Flex key={answerIndex}>
+              <FlexBlock>
+                <TextControl
+                  className="answerText"
+                  value={props.attributes.AnswersArray[answerIndex]}
+                  onChange={(newValue) => {
+                    const newArray = props.attributes.AnswersArray.concat([]);
+                    newArray[answerIndex] = newValue;
+                    props.setAttributes({
+                      AnswersArray: newArray,
+                    });
+                  }}
+                />
+              </FlexBlock>
+              <FlexItem>
+                <Button
+                  className={
+                    "iconButton" +
+                    (props.attributes.CorrectAnswer === answerIndex
+                      ? " correctAnswer "
+                      : "")
+                  }
+                  onClick={() => {
+                    props.setAttributes({
+                      CorrectAnswer: answerIndex,
+                    });
+                  }}
+                >
+                  <Icon className="selectAnswerIcon" icon="marker" />
+                </Button>
+              </FlexItem>
+              <FlexItem>
+                <Button
+                  className="iconButton"
+                  onClick={() => {
+                    onDeleteClick(answerIndex);
+                  }}
+                >
+                  <Icon className="deleteAnswerIcon" icon="trash" />
+                </Button>
+              </FlexItem>
+            </Flex>
+          );
+        }
+      )}
+      <Button isPrimary onClick={onAddAnswerClick}>
+        Add another answer
+      </Button>
+    </div>
+  );
 }
 
 function SaveComponent(props) {
-	return (
-		<div className='quizPlaceholder'>
-			{props.attributes.AnswersArray}
-		</div>
-	);
+  return (
+    <div className="quickQuiz_quizPlaceholder">
+      <p id="quickQuiz_data_question">{props.attributes.Question}</p>
+      <p id="quickQuiz_data_answers">
+        {props.attributes.AnswersArray.toString()}
+      </p>
+      <p id="quickQuiz_data_correct">{props.attributes.CorrectAnswer}</p>
+    </div>
+  );
 }
--- src/quizOutput.js
+++ src/quizOutput.js
@@ -1,31 +1,54 @@
 import "./quizOutput.scss";
 import React from "react";
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
-const QuizFront = (props) => {
+const QuizFront = ({ props }) => {
+  const [haveGuessed, setHaveGuessed] = useState("");
+  const onAnswerClick = (answerIndex) => {
+    if (answerIndex == props.correctAnswer) {
+      setHaveGuessed(true);
+    } else {
+      setHaveGuessed(false);
+    }
+  };
+
   return (
-    <div className="anwserWrapper">
-     {/*  {props.answers.map((answer) => {
+    <div className="quickQuiz_quizFrontWrapper">
+      {haveGuessed === true && <div className="quizFrontOverLay"></div>}
+      <h3 className="questionHeading">{props.question}</h3>
+      {props.answers.map((answer, answerIndex) => {
         return (
-          <div className="answer">
-            <p>{answer.toString()}</p>
-          </div>
+          <button
+            onClick={() => {
+              onAnswerClick(answerIndex);
+            }}
+            className="answerButton"
+            key={answerIndex}
+          >
+            {answer}
+          </button>
         );
-      })} */}
-      <p>{props.text + " Front"}</p>
+      })}
     </div>
   );
 };
 
 const onDOMContentLoaded = () => {
   const placeholderSelector = [].slice.call(
-    document.getElementsByClassName("quizPlaceholder")
+    document.getElementsByClassName("quickQuiz_quizPlaceholder")
   );
-  
+
   placeholderSelector.forEach((element) => {
-    let props = element.innerHTML; 
-    ReactDOM.render(<QuizFront text={props} />, element);
-    element.classList.remove("quizPlaceholder");
+    const question = element.children[0].textContent;
+    const answers = element.children[1].textContent.split(",");
+    const correctAnswer = element.children[2].textContent;
+
+    ReactDOM.render(
+      <QuizFront props={{ question, answers, correctAnswer }} />,
+      element
+    );
+    element.classList.remove("quickQuiz_quizPlaceholder");
   });
 };
 
